c6cdf920133235f7b5f595fe4b07a9a1ed93b75d
unknown <banyavariakos@gmail.com>
2019-02-11 22:09:27 +0100

    deploy

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=issue_tracker',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=dz8959rne9lumkkw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=w43exnp0ba520sdo',
+    'username' => 'kvb13g49190ylzfy',
+    'password' => 'kvcc7lsng3kwyv8e',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
